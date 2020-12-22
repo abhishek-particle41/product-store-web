5cff73e6ce7a38b181cbc9135df7ec22867f782d
Abhishek <abhishek.deshpande@particle41.com>
2020-12-22 12:05:32 +0530

    Add home and cart icons

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import '../Navbar/Navbar.css'
+import { Home, ShoppingCart } from '@material-ui/icons';
 
 function Navbar() {
     return (
         <div className="nav">
             <ul>
-                <Link to='/details'>
-                    <li className="nav-links">Home</li>
+                <Link to='/'>
+                    <Home fontSize="large" />
                 </Link>
                 <Link to='/cart'>
-                    <li className="nav-links">Cart</li>
+                    <ShoppingCart fontSize="large" />
                 </Link>
             </ul>
         </div>
