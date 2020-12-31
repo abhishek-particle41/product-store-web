026d48c3d36bdd93125893e25310bc2125730b97
Abhishek <abhishek.deshpande@particle41.com>
2020-12-31 19:35:38 +0530

    Navigate to details screen

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./style.scss";
 import { product } from './IProduct'
+import { NavLink } from 'react-router-dom'
 
 interface Props {
   product: product;
@@ -9,21 +10,23 @@ interface Props {
 
 const ProductView: React.FunctionComponent<Props> = ({ product }) => {
   return (
-    <div
-      className="item"
-    >
-      <div className="thumb">
-        <img src={product.image}
-          alt={product.title} title={product.title} />
-      </div>
-      <p className="title">{product.title}</p>
-      <div className="price">
-        <div className="val">
-          <b> $ {product.price}</b>
+    <NavLink className="item" to="/details" >
+      <div
+      // className="item"
+      >
+        <div className="thumb">
+          <img src={product.image}
+            alt={product.title} title={product.title} />
         </div>
+        <p className="title">{product.title}</p>
+        <div className="price">
+          <div className="val">
+            <b> $ {product.price}</b>
+          </div>
+        </div>
+        <div className="buy-btn">Add to cart</div>
       </div>
-      <div className="buy-btn">Add to cart</div>
-    </div>
+    </NavLink>
   );
 };
 
