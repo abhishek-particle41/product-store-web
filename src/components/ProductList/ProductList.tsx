5b9484268dab989d32a6c1a70a9f0ab26359ea94
Abhishek <abhishek.deshpande@particle41.com>
2021-02-04 14:23:04 +0530

    Add loader on home screen

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -4,13 +4,21 @@ import { RootStore } from "../../utils/store/store";
 import { GetItem } from "../../utils/store/Item/itemActions";
 import ProductView from '../Product/Product';
 import { Product } from '../../utils/store/types';
-import { Spinner } from 'react-bootstrap';
+import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 function DisplayData(props: { isLoading: boolean; items?: any; searchItem: string; category: string }) {
     const { isLoading, items, searchItem, category } = props
     const priceRange: any = useSelector((state: RootStore) => state.priceReducer);
     if (isLoading) {
-        return <Spinner animation="border" />;
+        return <div className="item-loader">
+            <Loader
+                type="ThreeDots"
+                color="#6d76f7"
+                height={100}
+                width={100}
+            />
+        </div>
     } else return <div className="shelf-container">
         {items && items.map((value: Product, index: number) => {
             if (value.title.includes(searchItem) && (category.length === 0 || category.includes(value.category)) && value.price >= priceRange[0] && value.price <= priceRange[1]) {
@@ -27,9 +35,7 @@ function ProductList() {
     const itemState = useSelector((state: RootStore) => state.itemReducer);
     const searchItemState = useSelector((state: RootStore) => state.searchItemReducer);
     const category: any = useSelector((state: RootStore) => state.categoriesReducer);
-    // const priceRange: any = useSelector((state: RootStore) => state.priceReducer);
     useEffect(() => {
-        // console.log(priceRange)
         if (itemState.item === undefined) { dispatch(GetItem()) }
     }, [])
     return (
