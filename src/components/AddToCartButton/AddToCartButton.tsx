1c7a9ace09a1187ff222c25cc2e80ee426217f3c
Abhishek <abhishek.deshpande@particle41.com>
2021-02-07 23:37:08 +0530

    Add to cart condition on details screen

--- src/components/AddToCartButton/AddToCartButton.tsx
+++ src/components/AddToCartButton/AddToCartButton.tsx
@@ -1,18 +1,39 @@
+import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { addToCart } from "../../utils/store/Product/productActions";
+import { RootStore } from "../../utils/store/store";
 import { Product } from '../../utils/store/types'
 import './style.scss'
 
+enum button {
+    Add = "Add to cart",
+    Added = "Added to cart",
+}
+
 function AddToCartButton(product: Product) {
     const dispatch = useDispatch();
+    const cartArray: any = useSelector((state: RootStore) => state.productReducer)
+    const [buttonText, setButtonText] = useState(button.Add)
+
+    useEffect(() => {
+        for (var i = 0; i < cartArray.length; i++) {
+            if (cartArray[i].id === product.id) {
+                setButtonText(button.Added)
+                break;
+            }
+        }
+    }, [])
 
     const onAddToCart = () => {
-        dispatch(addToCart(product));
+        if (buttonText === button.Add) {
+            dispatch(addToCart(product));
+            setButtonText(button.Added)
+        }
     };
 
     return (
         <button className="cart" onClick={() => { onAddToCart() }}>
-            Add To Cart
+            {buttonText}
         </button>
     );
 }
